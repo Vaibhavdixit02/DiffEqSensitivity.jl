5eb81548fa61e6e187a3b6d6e8aab0c1c77bbf8f
Unknown <vaibhavyashdixit@gmail.com>
2018-08-19 02:42:30 +0530

    Export regression_sensitivity

--- src/DiffEqSensitivity.jl
+++ src/DiffEqSensitivity.jl
@@ -12,11 +12,13 @@ include("local_sensitivity.jl")
 include("adjoint_sensitivity.jl")
 include("morris_sensitivity.jl")
 include("sobol_sensitivity.jl")
+include("regression_sensitivity.jl")
 
 export extract_local_sensitivities
 
 export ODELocalSensitvityFunction, ODELocalSensitivityProblem, SensitivityFunction,
        ODEAdjointSensitivityProblem, ODEAdjointProblem, AdjointSensitivityIntegrand,
-       adjoint_sensitivities, morris_sensitivity, MorrisSensitivity, sobol_sensitivity
+       adjoint_sensitivities, morris_sensitivity, MorrisSensitivity, sobol_sensitivity,
+       regression_sensitivity, Regression_Sensitivity_Coefficients
 
 end # module
